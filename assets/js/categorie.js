875c84b23d97ebb361fa9d1043dbe95aaf2286e8
PhilippeMath49 <philippe.mathieu@ynov.com>
2023-06-13 17:17:49 +0200

    js categories

--- assets/js/categorie.js
+++ assets/js/categorie.js
@@ -1,24 +1,24 @@
 let plus = document.querySelector("#plus");
-let newCat = document.querySelector("#new-cat");
-let titleInput = document.querySelector("#new-cat-title");
+let newTopic = document.querySelector("#new-topic");
+let titleInput = document.querySelector("#new-topic-title");
 let messageInput = document.querySelector("#first-message");
-let submit = document.querySelector("#new-cat-subbmit");
-let catList = document.querySelector("#cat-list");
+let submit = document.querySelector("#new-topic-subbmit");
+let topicList = document.querySelector("#topic-list");
 plus.addEventListener('click', displayNewCat);
 submit.addEventListener('click', submitData);
 
 function displayNewCat() {
   console.log("click confirmed")
-  newCat.style.display = 'block';
+  newTopic.style.display = 'block';
 }
 
 function submitData() {
   let message = messageInput.value;
   let title = titleInput.value;
 
-  catList.innerHTML += `
+  topicList.innerHTML += `
     <div class="box_ctn">
-      <img src="https://cdn.discordapp.com/attachments/1022765930511605791/1115953424169717912/1f3c0.png" alt="">
+      
       <div class="box_content">
         <h3>${title}</h3>
         <div class="content-ctn">
@@ -27,7 +27,7 @@ function submitData() {
       </div>
     </div>`;
 
-  newCat.style.display = 'none';
+  newTopic.style.display = 'none';
   messageInput.value = '';
   titleInput.value = '';
 }
